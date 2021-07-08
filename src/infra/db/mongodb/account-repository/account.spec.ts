3f02f85722976bec7ed0bd15559445c81c1e50d2
Binner <lucian.binner@hotmail.com>
2021-07-07 21:05:33 -0300

    refactor: move sut creation to a factory helper methods

--- src/infra/db/mongodb/account-repository/account.spec.ts
+++ src/infra/db/mongodb/account-repository/account.spec.ts
@@ -5,11 +5,17 @@ describe('Account Mongo Repository', () => {
     beforeAll(async () => {
         await MongoHelper.connect(process.env.MONGO_URL as string)
     })
+
     afterAll(async () => {
         await MongoHelper.disconect()
     })
+
+    const makeSut = (): AccountMongoRepository => {
+        return new AccountMongoRepository()
+    }
+
     test('Should return an account on success', async () => {
-        const sut = new AccountMongoRepository()
+        const sut = makeSut()
         const account = await sut.add({
             name: 'any_name',
             email: 'any_email@mail.com',
