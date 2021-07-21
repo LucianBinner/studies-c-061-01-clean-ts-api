617460033072fd697c58354c1d3f689589c90457
Binner <lucian.binner@hotmail.com>
2021-07-21 15:09:43 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,8 +1,8 @@
 import { InvalidParamError, MissingParamError } from "../../errors";
-import { badRequest, serverError, unauthorizedError } from "../../helpers/http-helper";
+import { badRequest, ok, serverError, unauthorizedError } from "../../helpers/http-helper";
 import { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from "./login-protocols";
 
-export class LoginController implements Controller{
+export class LoginController implements Controller {
     private readonly emailValidator: EmailValidator
     private readonly authentication: Authentication
     constructor(emailValidator: EmailValidator, authentication: Authentication) {
@@ -11,25 +11,20 @@ export class LoginController implements Controller{
     }
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
-            const {email, password} = httpRequest.body
+            const { email, password } = httpRequest.body
             const requiredField = ['email', 'password']
             for (const field of requiredField) {
-                if (!httpRequest.body[field]) 
+                if (!httpRequest.body[field])
                     return badRequest(new MissingParamError(field))
             }
             const isValid = this.emailValidator.isValid(email)
-            if(!isValid) 
+            if (!isValid)
                 return badRequest(new InvalidParamError('email'))
             const accessToken = await this.authentication.auth(email, password)
-            if(!accessToken) {
+            if (!accessToken) {
                 return unauthorizedError()
             }
-            return {
-                statusCode: 200,
-                body: {
-                    message: 'Success'
-                }
-            }
+            return ok({ accessToken })
         } catch (error) {
             return serverError(error)
         }
