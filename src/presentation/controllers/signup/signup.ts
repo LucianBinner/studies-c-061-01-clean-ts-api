fd0f6470362905690a255a636cc62d793ef8cc13
Binner <lucian.binner@hotmail.com>
2021-07-22 17:19:03 -0300

    refactor: move EmailValidation to the ValidationComposite

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,18 +1,15 @@
-import { HttpRequest, HttpResponse, Controller, EmailValidator, AddAccount, Validation } from './signup-protocols'
+import { HttpRequest, HttpResponse, Controller, AddAccount, Validation } from './signup-protocols'
 import { InvalidParamError } from '../../errors'
 import { badRequest, serverError, ok } from '../../helpers/http-helper'
 
 export class SignUpController implements Controller {
-    private readonly emailValidator: EmailValidator
     private readonly addAccount: AddAccount
     private readonly validation: Validation
-
-    constructor(emailValidator: EmailValidator, addAccount: AddAccount, validation: Validation) {
-        this.emailValidator = emailValidator
+    constructor(addAccount: AddAccount, validation: Validation) {
+        
         this.addAccount = addAccount
         this.validation = validation
     }
-
     async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
         try {
             const error = this.validation.validate(httpRequest.body)
@@ -20,12 +17,7 @@ export class SignUpController implements Controller {
                 return badRequest(error)
             }
             const { name, email, password } = httpRequest.body
-            const isValid = this.emailValidator.isValid(email)
-            if (!isValid) {
-                return badRequest(new InvalidParamError('email'))
-            }
             const account = await this.addAccount.add({ name, email, password })
-
             return ok(account)
         } catch (error) {
             return serverError(error)
