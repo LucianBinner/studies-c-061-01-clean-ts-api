323b1852302173dc554274559eb9fe469d2fc8aa
Binner <lucian.binner@hotmail.com>
2021-07-27 21:09:44 -0300

    test: ensure SignUpController returns 403 if AddAccount returns null

--- src/presentation/controllers/signup/signup-controller.spec.ts
+++ src/presentation/controllers/signup/signup-controller.spec.ts
@@ -1,5 +1,5 @@
 import { SignUpController } from "./signup-controller"
-import { MissingParamError, ServerError } from '../../errors'
+import { EmailInUseError, MissingParamError, ServerError } from '../../errors'
 import { 
     AddAccount, 
     AddAccountModel, 
@@ -9,11 +9,11 @@ import {
     AuthenticationModel 
 } from "./signup-controller-protocols"
 import { HttpRequest } from "../../protocols"
-import { ok, serverError, badRequest } from "../../helpers/http/http-helper"
+import { ok, serverError, badRequest, forbidden } from "../../helpers/http/http-helper"
 
 const makeAddAccount = (): AddAccount => {
     class AddAccountStub implements AddAccount {
-        async add(account: AddAccountModel): Promise<AccountModel> {
+        async add(account: AddAccountModel): Promise<AccountModel | null> {
             return new Promise(resolve => resolve(makeFakeAccount()))
         }
     }
@@ -94,6 +94,13 @@ describe('SignUp Controllers', () => {
         })
         const httpResponse = await sut.handle(makeFakeRequest())
         expect(httpResponse).toEqual(serverError(new ServerError('Server error!')))
+    })
+
+    test('Should return 403 if AddAccount returns null', async () => {
+        const { sut, addAccountStub } = makeSut()
+        jest.spyOn(addAccountStub, 'add').mockReturnValueOnce(new Promise(resolve => resolve(null)))
+        const httpResponse = await sut.handle(makeFakeRequest())
+        expect(httpResponse).toEqual(forbidden(new EmailInUseError()))
     })
 
     test('Should return 200 if valid data is provided', async () => {
