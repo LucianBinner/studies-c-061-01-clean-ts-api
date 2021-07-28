6ca9ed184054a158c0055fd7da41d65c32b840b7
Binner <lucian.binner@hotmail.com>
2021-07-28 08:58:17 -0300

    refactor: move login and signup controllers to a folder

--- src/presentation/controllers/login/login/login-controller.spec.ts
+++ src/presentation/controllers/login/login/login-controller.spec.ts
@@ -1,9 +1,9 @@
 import { Authentication, HttpRequest } from "./login-controller-protocols";
-import { MissingParamError } from "../../errors";
-import { badRequest, ok, serverError, unauthorizedError } from "../../helpers/http/http-helper";
+import { MissingParamError } from "../../../errors";
+import { badRequest, ok, serverError, unauthorizedError } from "../../../helpers/http/http-helper";
 import { LoginController } from "./login-controller";
 import { Validation } from "../signup/signup-controller-protocols";
-import { AuthenticationModel } from "../../../domain/usecases/authentication";
+import { AuthenticationModel } from "../../../../domain/usecases/authentication";
 
 const makeAuthentication = (): Authentication => {
     class AuthenticationStub implements Authentication {
