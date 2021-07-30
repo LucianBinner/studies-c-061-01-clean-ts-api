c8db320addae68fcfdad0eb4572a20c1c86d6d1d
Binner <lucian.binner@hotmail.com>
2021-07-30 11:07:12 -0300

    test: ensure DbLoadSurveys throws if LoadSurveyRepository throws

--- src/data/usecases/load-surveys/db-load-surveys.spec.ts
+++ src/data/usecases/load-surveys/db-load-surveys.spec.ts
@@ -59,4 +59,14 @@ describe('DbLoadSurveys ', () => {
     const surveys = await sut.load()
     expect(surveys).toEqual(makeFakeSurveys())
   })
+
+  test('Should throw if LoadSurveyRepository throws', async () => {
+    const { sut, loadSurveysRepositoryStub } = makeSut()
+    jest.spyOn(loadSurveysRepositoryStub, 'loadAll').mockReturnValueOnce(new Promise(
+      (_, reject) =>
+        reject(new Error())
+    ))
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow()
+  })
 })
