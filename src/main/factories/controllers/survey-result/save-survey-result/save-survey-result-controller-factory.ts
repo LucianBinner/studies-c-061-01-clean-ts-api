ce70fee75eec11318fa2cf26df9ebd894ac7f959
Binner <lucian.binner@hotmail.com>
2021-07-31 14:20:26 -0300

    feat: ensure save survey result route returns 403  if no token is provided

---
+++ src/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory.ts
@@ -0,0 +1,13 @@
+import { makeLogControllerDecorator } from "@/main/factories/decorators/log-controller-decorator-factory";
+import { makeDbSaveSurveyResult } from "@/main/factories/usecases/survey-result/save-survey-resukt/db-save-survey-result-factory";
+import { makeDbLoadSurveyById } from "@/main/factories/usecases/survey/load-survey-by-id/db-load-surveys-by-id-factory";
+import { SaveSurveyResultController } from "@/presentation/controllers/survey-result/save-survey-result/save-survey-result-controller";
+import { Controller } from "@/presentation/protocols";
+
+export const makeSaveSurveyResultController = (): Controller => {
+    const saveSurveyResultController = new SaveSurveyResultController(
+        makeDbLoadSurveyById(), 
+        makeDbSaveSurveyResult()
+    )
+    return makeLogControllerDecorator(saveSurveyResultController)
+}
