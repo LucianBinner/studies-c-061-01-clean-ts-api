4790e40668611af0988c28e76708e3e57e1e70f7
Binner <lucian.binner@hotmail.com>
2021-08-02 20:08:49 -0300

    refactor: delete of unused variables

--- src/main/server.ts
+++ src/main/server.ts
@@ -4,7 +4,6 @@ import { MongoHelper } from "@/infra/db/mongodb/helpers/mongo-helper";
 
 MongoHelper.connect(env.mongoUrl)
     .then(async () => {
-        const name = 'teste'
         const app = (await import("./config/app")).default
         app.listen(env.port, () => console.log(`Server is running at http://localhost:${env.port}`))
     })
