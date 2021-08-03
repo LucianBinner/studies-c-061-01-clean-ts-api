0c1b3d92aecf2c5ea2e05cbf62986d4244694b56
Binner <lucian.binner@hotmail.com>
2021-08-03 19:38:28 -0300

    feat: ensure LoadSurveyResultController calls LoadSurveyResult with correct value

--- src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
+++ src/presentation/controllers/survey-result/load-survey-result/load-survey-result-controller-protocols.ts
@@ -2,4 +2,6 @@ export * from "@/presentation/protocols"
 export * from "@/domain/models/survey"
 export * from "@/domain/usecases/survey/load-survey-by-id"
 export * from "@/presentation/protocols"
-export * from "@/domain/usecases/survey/load-survey-by-id";
+export * from "@/domain/usecases/survey/load-survey-by-id"
+export * from "@/domain/usecases/survey-result/load-survey-result"
+
