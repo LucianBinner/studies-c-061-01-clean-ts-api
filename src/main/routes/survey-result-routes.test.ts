873cbbafc33f8cb5e977d88f3821ccbcb2169e33
Binner <lucian.binner@hotmail.com>
2021-08-03 20:04:47 -0300

    test: ensure load survey rotereturns 200 on success

--- src/main/routes/survey-result-routes.test.ts
+++ src/main/routes/survey-result-routes.test.ts
@@ -80,5 +80,23 @@ describe('Survey Routes', () => {
                 .get('/api/surveys/any_id/results')
                 .expect(403)
         })
+
+        test('Should return 200 on load survey result with accessToken', async () => {
+            const accessToken = await makeAccessToken()
+            const res = await surveyCollection.insertOne({
+                question: 'question',
+                answers: [{
+                    answer: 'answer_one',
+                    image: 'http://image-name.com'
+                }, {
+                    answer: 'answer_two'
+                }],
+                date: new Date()
+            })
+            await request(app)
+                .get(`/api/surveys/${res.ops[0]._id}/results`)
+                .set('x-access-token', accessToken)
+                .expect(200)
+        })
     })
 })
